222e87d20c33bb8859ff5f6c948d778f70b88528
Jon Zgoda <jon@zgoda.us>
2022-04-22 08:45:17 -0500

    Simple animation working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
+import { trigger, transition, style, animate } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('leaveFade', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('250ms', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate('250ms', style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
-  title = 'leaveFade';
+  show: boolean = false;
+
+  click(): void {
+    this.show = !this.show;
+  }
 }
